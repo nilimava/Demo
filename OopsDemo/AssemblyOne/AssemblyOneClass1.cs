45ae550b96359380200e18886935378ca26279a1
NilimaVadarevu <mangsule.nilima123@gmail.com>
2026-02-14 12:29:41 +0000

    Initial Commit

---
+++ OopsDemo/AssemblyOne/AssemblyOneClass1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        private int Id;
+        public void Display1()
+        {
+            //Private Member Accessible with the Containing Type only
+            //Where they are created, they are available only within that type
+            Console.WriteLine(Id);
+        }
+    }
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            Display1();
+        }
+
+        
+     
+        //You cannot access the Private Member from the Derived Class
+        //Within the Same Assembly
+        // Console.WriteLine(Id); //Compile Time Error
+    }
+
+   
+}
+
---
+++ OopsDemo/OopsDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Remoting;
+using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace OopsDemo
+{
+  
+   public class Modifiers
+    {
+       
+        static void Main(string[] args)
+        {
+            //reference type -pass by value
+
+            Person person = new Person(){ Name = "John"};
+
+            UpdateName(person); //passing reference (person) to method
+            Console.WriteLine(person.Name);
+            Console.ReadKey();
+        }
+
+        public static void UpdateName(Person p) //ref of person is copied into p
+        {
+            //p = new Person { Name = "Max" }; //now  p is pointing to new object instead of old object
+            p.Name = "Max";
+        }
+    }
+}
+
+
+
+
